7b2abbebd954e6d376baba7de7439c8da3b3e6f7
Michael Abbott <michael@mabbott.dev>
2024-04-08 22:26:08 +0100

    chore: remove tonumber

--- variables.tf
+++ variables.tf
@@ -4,8 +4,9 @@ variable "organization_name" {
 }
 
 variable "github_app_installation_id" {
-  default = 0123456789
-  type    = string
+  default     = 0123456789
+  type        = string
+  description = "github app installation id instaled by terraform cloud"
 }
 
 variable "github_organization_name" {
